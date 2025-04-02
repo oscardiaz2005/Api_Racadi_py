3fe52db0552da5fdfc2ecf50ad28319ef00a1c23
Oscar Diaz <oscaritho2005@gmail.com>
2025-04-01 20:55:29 -0500

    merge with main

--- core/conexion.py
+++ core/conexion.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:core/conexion.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -19,30 +18,4 @@ def get_db():
     try:
         yield conex
     finally:
-=======
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-
-#url de la base de datos , toca que cambien la contraseña a la de ustedes cada vez que quieran probar
-
-
-
-URL_DB="mysql+mysqlconnector://root:osjdiaz123@localhost:3306/racadi_academy"
-
-URL_DB="mysql+mysqlconnector://root:sebas1531@localhost:3306/racadi_academy"
-
-
-crear=create_engine(URL_DB)
-SessionLocal=sessionmaker(autocommit=False,autoflush=False, bind=crear)
-base=declarative_base()
-
-
-def get_db():
-    conex=SessionLocal()
-    try:
-        yield conex
-    finally:
->>>>>>> sebas:conexion.py
         conex.close()
